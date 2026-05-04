7433657c2a46364a800317df8dd44cf2a652a0be
2Grifon <evgeny.suraykin@gmail.com>
2026-05-04 23:25:47 +0300

    upd - Changed currency field in "Item" model to use choices

--- backend/app/apps/shop/models/item.py
+++ backend/app/apps/shop/models/item.py
@@ -1,13 +1,17 @@
-from django.db.models import Model, CharField, PositiveIntegerField, TextField
+from django.db.models import Model, CharField, PositiveIntegerField, TextChoices, TextField
 
 
 class Item(Model):
+    class CurrencyChoices(TextChoices):
+        USD = "usd", "USD"
+        RUB = "rub", "RUB"
+
     name = CharField(max_length=255)
     description = TextField()
     price = PositiveIntegerField(
         help_text="Цена в наименьших единицах валюты (копейки, центы и т.д.)"
     )
-    currency = CharField(max_length=3, default="usd")  # Сделать через choices?
+    currency = CharField(choices=CurrencyChoices.choices, max_length=3, default="usd")
 
     def __str__(self):
         return self.name
